0b80ce684ef931b7be6068bb9d157c814456b9bc
Hector Nava <hector.r.nava44@gmail.com>
2022-04-27 15:08:02 -0600

    Formatted functions

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -10,10 +10,10 @@ export const Contact = () => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
-          email: data.get('email'),
-          password: data.get('password'),
+            email: data.get('email'),
+            password: data.get('password'),
         });
-      };
+    };
 
     return(
         <Fragment>

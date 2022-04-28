50b009c32f5faf2e089dd74bd7a06adaff279dd3
Hector Nava <hector.r.nava44@gmail.com>
2022-04-28 11:43:45 -0600

    Added links to applications

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -2,40 +2,48 @@ import { Fragment } from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ButtonGroup from '@mui/material/ButtonGroup';
 import IconButton from '@mui/material/IconButton';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import Title from './Title';
 
 const itemData = [
     {
         name: 'socialmediaapp.gif',
         title: 'Social Media App',
-        repo: 'https://github.com/hnava47/SocialMediaAPI'
+        repo: 'https://github.com/hnava47/SocialMediaAPI',
+        url: 'https://socialmediap2.herokuapp.com/'
     },
     {
         name: 'ecommerceapp.gif',
         title: 'E-Commerce App',
-        repo: 'https://github.com/hnava47/ECommerceApp'
+        repo: 'https://github.com/hnava47/ECommerceApp',
+        url: 'https://ecommerceapp-p3.herokuapp.com/'
     },
     {
         name: 'weatherdashboard.gif',
         title: 'Weather Dashboard',
-        repo: 'https://github.com/hnava47/WeatherDashboardH6'
+        repo: 'https://github.com/hnava47/WeatherDashboardH6',
+        url: 'https://hnava47.github.io/WeatherDashboardH6/'
     },
     {
         name: 'booksearchengine.gif',
         title: 'Book Search Engine',
-        repo: 'https://github.com/hnava47/BookSearchEngine'
+        repo: 'https://github.com/hnava47/BookSearchEngine',
+        url: 'https://searchengine-hn.herokuapp.com/'
     },
     {
         name: 'codequiz.gif',
         title: 'Code Quiz',
-        repo: 'https://github.com/hnava47/CodeQuizH4'
+        repo: 'https://github.com/hnava47/CodeQuizH4',
+        url: 'https://hnava47.github.io/CodeQuizH4/'
     },
     {
         name: 'passwordgenerator.gif',
         title: 'Password Generator',
-        repo: 'https://github.com/hnava47/PasswordGeneratorH3'
+        repo: 'https://github.com/hnava47/PasswordGeneratorH3',
+        url: 'https://hnava47.github.io/PasswordGeneratorH3/'
     }
   ];
 
@@ -55,15 +63,26 @@ export const Project = () => {
                         <ImageListItemBar
                             title={item.title}
                             actionIcon={
-                            <IconButton
-                                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                                component='a'
-                                href={`${item.repo}`}
-                                target='_blank'
-                                aria-label={`Github repo about ${item.title}`}
-                            >
-                                <GitHubIcon />
-                            </IconButton>
+                                <ButtonGroup>
+                                    <IconButton
+                                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                        component='a'
+                                        href={`${item.url}`}
+                                        target='_blank'
+                                        aria-label={`Exit to ${item.title}`}
+                                    >
+                                        <ExitToAppIcon />
+                                    </IconButton>
+                                    <IconButton
+                                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                                        component='a'
+                                        href={`${item.repo}`}
+                                        target='_blank'
+                                        aria-label={`Github repo about ${item.title}`}
+                                    >
+                                        <GitHubIcon />
+                                    </IconButton>
+                                </ButtonGroup>
                             }
                         />
                     </ImageListItem>
